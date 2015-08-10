bf1b9f3ba0c9a7215aac5eb9b705815b97fac429
Sushant Choudhary <schoudhary@atlassian.com>
2015-08-10 20:31:16 +0700

    Initial Commit

---
+++ XCTestDoclet/XCTestDoclet.h
@@ -0,0 +1,19 @@
+//
+//  XCTestDoclet.h
+//  XCTestDoclet
+//
+//  Created by Sushant Choudhary on 8/10/15.
+//  Copyright © 2015 Sushant Choudhary. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for XCTestDoclet.
+FOUNDATION_EXPORT double XCTestDocletVersionNumber;
+
+//! Project version string for XCTestDoclet.
+FOUNDATION_EXPORT const unsigned char XCTestDocletVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <XCTestDoclet/PublicHeader.h>
+
+
---
+++ XCTestDocletTests/XCTestDocletTests.m
@@ -0,0 +1,39 @@
+//
+//  XCTestDocletTests.m
+//  XCTestDocletTests
+//
+//  Created by Sushant Choudhary on 8/10/15.
+//  Copyright © 2015 Sushant Choudhary. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface XCTestDocletTests : XCTestCase
+
+@end
+
+@implementation XCTestDocletTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
